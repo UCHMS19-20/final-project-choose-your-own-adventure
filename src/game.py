99997ee34ad4fa24c526a832773985b6141ef0aa
Kristen <kadams1@ucvts.org>
2019-12-16 13:17:58 -0500

    Added intro text

--- src/game.py
+++ src/game.py
@@ -2,27 +2,27 @@ import time
 
 print("We are pleased to inform you that you have been accepted into Hollywood Arts High School! \n")
 
-time.sleep(5)
+time.sleep(3)
 
 print("You came to the realization that engineering wasn't your true calling in life, but performing arts instead! \n")
 
-time.sleep(5)
+time.sleep(3)
 
 print("Every year, Hollywood Arts hosts an event called 'The Big Showcase,' where agents and talent spotters around the country are invited. \n")
 
-time.sleep(5)
+time.sleep(3)
 
 print("It is your job to prepare a showstopping performance by the end of this week. \n")
 
-time.sleep(5)
+time.sleep(3)
 
 print("But to do this, you will need to get all of your materials together before the show. You will need to make friends, make smart choices, and make sure you don't run out of turns! \n")
 
-time.sleep(5)
+time.sleep(3)
 
 print("Before you can begin your adventure in Hollywood Arts, we will need to know more about you. \n")
 
-time.sleep(5)
+time.sleep(3)
 
 name = input("What is your name? ")
 
