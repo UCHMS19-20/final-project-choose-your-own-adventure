4c42f64157d381ad22458edda17aa786e7d0f168
Kristen <kadams1@ucvts.org>
2019-12-16 13:13:19 -0500

    Added intro text

--- src/game.py
+++ src/game.py
@@ -1,27 +1,29 @@
 import time
 
-print("We are pleased to inform you that you have been accepted into Hollywood Arts High School!")
+print("We are pleased to inform you that you have been accepted into Hollywood Arts High School! \n")
 
-time.sleep(1)
+time.sleep(5)
 
-print("You came to the realization that engineering wasn't your true calling in life, but performing arts instead!")
+print("You came to the realization that engineering wasn't your true calling in life, but performing arts instead! \n")
 
-time.sleep(1)
+time.sleep(5)
 
-print("Every year, Hollywood Arts hosts an event called 'The Big Showcase,' where agents and talent spotters around the country are invited.")
+print("Every year, Hollywood Arts hosts an event called 'The Big Showcase,' where agents and talent spotters around the country are invited. \n")
 
-time.sleep(2)
+time.sleep(5)
 
-print("It is your job to prepare a showstopping performance by the end of this week.")
+print("It is your job to prepare a showstopping performance by the end of this week. \n")
 
-time.sleep(2)
+time.sleep(5)
 
-print("But to do this, you will need to get all of your materials together before the show. You will need to make friends, make smart choices, and make sure you don't run out of turns!")
+print("But to do this, you will need to get all of your materials together before the show. You will need to make friends, make smart choices, and make sure you don't run out of turns! \n")
 
-time.sleep(3)
+time.sleep(5)
 
-print("Before you can begin your adventure in Hollywood Arts, we will need to know more about you.")
+print("Before you can begin your adventure in Hollywood Arts, we will need to know more about you. \n")
 
-time.sleep(1)
+time.sleep(5)
 
-print(input("What is your name?" ))
+name = input("What is your name? ")
+
+print(name)
